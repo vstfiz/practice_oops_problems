78071ec7348819d6f5d7bc0542098e993b3e7030
vstfiz <vivek.sharma7354@gmail.com>
2019-12-08 19:45:21 +0530

    added Programming.java

--- src/Number.java
+++ src/Number.java
@@ -1,22 +1,34 @@
+import java.util.Scanner;
+
 public class Number {
     private double number;
     public Number(double number){
         this.number = number;
     }
     public boolean isZero(double number){
-        return number==0;
+        boolean isZero;
+        isZero = Double.compare(number,Double.parseDouble("0"))==0?true:false;
+        return isZero;
     }
     public boolean isPositive(double number){
-        return number==0;
+        boolean isPositive;
+        isPositive = Double.compare(number, Double.parseDouble("0")) > 0;
+        return isPositive;
     }
     public boolean isNegative(double number){
-        return number==0;
+        boolean isNegative;
+        isNegative = Double.compare(number, Double.parseDouble("0")) < 0;
+        return isNegative;
     }
     public boolean isOdd(double number){
-        return number==0;
+        boolean isOdd;
+        isOdd = (Math.floor(number) % 2) != 0;
+        return isOdd;
     }
     public boolean isEven(double number){
-        return number==0;
+        boolean isEven;
+        isEven = (Math.floor(number) % 2) == 0;
+        return isEven;
     }
     public boolean isPrime(double number){
         boolean primeFlag = false;
@@ -26,10 +38,36 @@ public class Number {
                 count++;
             }
         }
-//        count == 1?primeFlag = true:primeFlag = false;
+        primeFlag = count == 0;
         return primeFlag;
     }
     public boolean isArmstrong(double number){
-        return number==0;
+        boolean isArmstrong;
+        int originalNumber = (int)number;
+        int originalNumber1 = (int)number;
+        int remainder, result = 0;
+        int lengthNumber = 0;
+        while(originalNumber1 != 0){
+            originalNumber1 = originalNumber1/10;
+            lengthNumber++;
+        }
+        System.out.println(lengthNumber);
+        while (originalNumber != 0)
+        {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, lengthNumber);
+            originalNumber /= 10;
+        }
+        isArmstrong = result == Math.floor(number);
+        return isArmstrong;
     }
+    //Driver Code to check the Definition Class Code.
+    // Un-comment it to check.
+
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        Number number = new Number(scanner.nextDouble());
+//        System.out.println(number.isArmstrong(number.number));
+//    }
 }
---
+++ src/Programming.java
@@ -0,0 +1,16 @@
+public class Programming {
+    public Programming(){
+        System.out.println("I love programming languages");
+    }
+    public Programming(String s){
+        System.out.println("I love "+s);
+    }
+
+    //Driver Code to check the Definition Class Code.
+    // Un-comment it to check.
+
+//    public static void main(String[] args) {
+//        Programming programming = new Programming();
+//        Programming programming1 = new Programming("Java");
+//    }
+}
